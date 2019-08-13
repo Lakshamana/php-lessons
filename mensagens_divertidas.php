b370057f5f23b83683ba0d7d5c0d3d6066c6dbe4
Lakshamana <guitrompa1@gmail.com>
2019-08-13 13:33:01 -0300

    echo and print

---
+++ mensagens_divertidas.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" type="text/css" href="estilo.css" />
+    <title>Mensagens divertidas</title>
+  </head>
+
+  <body>
+    <?php
+      echo 'teste echo<br/>';
+      print 'teste print'
+    ?>
+  </body>
+</html>
