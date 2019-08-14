f81e37141360fc43181efb69789a5e3ba85f99cb
Lakshamana <guitrompa1@gmail.com>
2019-08-13 22:34:40 -0300

    add artithmetic lesson

---
+++ arithmetic.php
@@ -0,0 +1,10 @@
+<?php
+    $n1 = 7;
+    $n2 = 3;
+    echo 7 + 3, '<br/>';
+    echo 7 - 3, '<br/>';
+    echo 7 * 3, '<br/>';
+    echo 7 / 3, '<br/>';
+    echo (int) (7 / 3), '<br/>';
+    echo 7 % 3, '<br/>';
+?>
